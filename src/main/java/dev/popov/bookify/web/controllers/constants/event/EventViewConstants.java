99735d294333ab7ac6a29355f73b6993c133abe6
Nikola-Popov <nikola.angelov.popov@gmail.com>
2019-04-22 02:37:40 +0300

    Removed leading slashes

--- src/main/java/dev/popov/bookify/web/controllers/constants/event/EventViewConstants.java
+++ src/main/java/dev/popov/bookify/web/controllers/constants/event/EventViewConstants.java
@@ -1,8 +1,8 @@
 package dev.popov.bookify.web.controllers.constants.event;
 
 public final class EventViewConstants {
-	public static final String BROWSE_EVENTS = "/event/browse_events";
-	public static final String PREVIEW_EVENT = "/event/preview_event";
-	public static final String ALL_EVENTS = "/event/all_events";
-	public static final String CREATE_EVENT = "/event/create_event";
+	public static final String BROWSE_EVENTS = "event/browse_events";
+	public static final String PREVIEW_EVENT = "event/preview_event";
+	public static final String ALL_EVENTS = "event/all_events";
+	public static final String CREATE_EVENT = "event/create_event";
 }

783e9c735bf68fc7543bba819347126c855b97a7
Nikola-Popov <nikola.angelov.popov@gmail.com>
2019-04-23 03:19:18 +0300

    Removed favicon interceptor

--- src/main/java/dev/popov/bookify/config/ApplicationWebMvcConfiguration.java
+++ src/main/java/dev/popov/bookify/config/ApplicationWebMvcConfiguration.java
@@ -5,24 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import dev.popov.bookify.web.interceptors.FaviconInterceptor;
 import dev.popov.bookify.web.interceptors.PageTitleInterceptor;
 
 @Configuration
 public class ApplicationWebMvcConfiguration implements WebMvcConfigurer {
 	private final PageTitleInterceptor pageTitleInterceptor;
-	private final FaviconInterceptor faviconInterceptor;
 
 	@Autowired
-	public ApplicationWebMvcConfiguration(PageTitleInterceptor pageTitleInterceptor,
-			FaviconInterceptor faviconInterceptor) {
+	public ApplicationWebMvcConfiguration(PageTitleInterceptor pageTitleInterceptor) {
 		this.pageTitleInterceptor = pageTitleInterceptor;
-		this.faviconInterceptor = faviconInterceptor;
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(pageTitleInterceptor);
-		registry.addInterceptor(faviconInterceptor);
 	}
 }
